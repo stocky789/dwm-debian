ce5ee8a9326529d9a98b4f7ffa637ec2458c3fb4
stocky789 <mstock96@hotmail.com>
2025-03-18 09:49:12 +1100

    Updated

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ read -p "Are you installing on a system with an NVIDIA GPU? (yes/no): " nvidia_c
 echo "Installing DWM package dependencies..."
 apt install -y \
     thunar xorg xserver-xorg curl firefox-esr xinit x11-xserver-utils feh picom gdm3 \
-    pavucontrol fonts-hack-ttf dwm libimlib2-dev fonts-font-awesome pamixer gamemode rofi flameshot wget \
+    pavucontrol fonts-hack-ttf dwm libimlib2-dev fonts-font-awesome adwaita-qt pamixer gamemode rofi flameshot wget \
     zsh timeshift pipewire pipewire-audio pipewire-alsa \
     kitty lxappearance network-manager-gnome dunst build-essential libx11-dev libxft-dev libxinerama-dev \
     curl unzip qt5ct qt6ct xfce4-settings
